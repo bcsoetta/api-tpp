5e74ae08dc2d85a3660ce76c1f11ad21a2fcae2f
Bow Vernon <bowvernon110391@gmail.com>
2020-07-15 14:50:49 +0700

    Finish api for penetapan

--- app/Http/Controllers/PenetapanController.php
+++ app/Http/Controllers/PenetapanController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\AppLog;
 use App\Penetapan;
+use App\SSOUserCache;
+use App\TPS;
 use App\Transformers\PenetapanTransformer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PenetapanController extends ApiController
 {
@@ -41,9 +45,73 @@ class PenetapanController extends ApiController
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $r, $kdTps)
     {
-        //
+        // store penetapan
+        DB::beginTransaction();
+        try {
+            $tps = TPS::byKode($kdTps)->first();
+
+            if (!$tps) {
+                throw new \Exception("TPS {$kdTps} tidak ditemukan");
+            }
+
+            // cache pejabat_id
+            $pejabat_id = expectSomething($r->get('pejabat_id'), "Pejabat Penetapan");
+
+            SSOUserCache::byId($pejabat_id);
+
+            // ok, now we make a new Penetapan
+            $p = new Penetapan([
+                'kode_kantor'   => '050100',
+                'nomor_lengkap_dok' => strtoupper(trim($r->get('nomor_lengkap_dok'))),
+                'tgl_dok' => expectSomething($r->get('tgl_dok'), 'Tanggal Surat Penetapan'),
+                'pejabat_id' => $pejabat_id
+            ]);
+
+            // save it
+            $p->save();
+            $p->appendStatus('CREATED');
+
+            // if number is empty, assign it
+            if (!$p->nomor_lengkap_dok) {
+                $p->setNomorDokumen();
+            }
+
+            // lock it?
+            $p->lock()->create([
+                'keterangan' => "Penetapan BTD untuk tps {$kdTps}",
+                'petugas_id' => $r->userInfo['user_id']
+            ]);
+            $p->appendStatus('LOCKED');
+
+            // log it?
+            AppLog::logInfo("Penetapan #{$p->id} direkam oleh {$r->userInfo['username']}", $p, false);
+
+            // now we fill the assignment
+            $ms = $tps->entryManifest()->siapPenetapan()->get();
+
+            // for each of them, add to penetapan
+            foreach ($ms as $m) {
+                // add to penetapan
+                $p->entryManifest()->save($m);
+
+                // append status
+                $m->appendStatus('PENETAPAN', null, null, $p);
+            }
+
+            // commit
+            DB::commit();
+
+            // return info on how many was assigned
+            return $this->respondWithArray([
+                'id' => (int) $p->id,
+                'total' => count($ms)
+            ]);
+        } catch (\Throwable $e) {
+            DB::rollBack();
+            return $this->errorBadRequest($e->getMessage());
+        }
     }
 
     /**
