84cd32776d3569c06bee3eb68a6b78431cce9781
Bow Vernon <bowvernon110391@gmail.com>
2020-07-20 19:52:55 +0700

    Add ringkasan_uraian_barang attribute to TraitHasGoods

--- app/TraitHasGoods.php
+++ app/TraitHasGoods.php
@@ -5,4 +5,18 @@ trait TraitHasGoods {
     public function detailBarang() {
         return $this->morphMany(DetailBarang::class, 'header');
     }
+
+    // attributes
+    public function getRingkasanUraianBarangAttribute() {
+        // just grab all detail barang and return the string
+        $ret = '';
+
+        $uraianPendek = $this->detailBarang->map(function($e) {
+            return ($e->jumlah ? (float)$e->jumlah . ' ' : '')
+                . ($e->jenis ? $e->jenis . ' ' : '')
+                . $e->uraian;
+        })->toArray();
+
+        return implode(";\n", $uraianPendek);
+    }
 }
